b149b46483ae25591d52213c9692363094616c09
Erol-web <81830899+Erol-web@users.noreply.github.com>
2022-06-24 13:21:15 +0300

    add

--- The-Learning-App/src/views/Learning.vue
+++ The-Learning-App/src/views/Learning.vue
@@ -25,8 +25,7 @@ export default ({
     data () {
         return {
             courseInformationArray: [],
-            currentCourseObject: {id: 0, courseName: '', description: '', lessons: 0, image: undefined },
-            idCounter: 0
+            currentCourseObject: {id: 0, courseName: '', description: '', lessons: 0, image: undefined }
         }
     },
 
@@ -37,11 +36,11 @@ export default ({
             let courseName = '';
             let description = '';
             let lessons = '';
-            let image = undefined;
+            // let image = document.createElement('img');
 
             while (!courseName) {
                  courseName = prompt('Add Course Name (required field)?');
-                 if (courseName !== '' && courseName !== null) {
+                if (courseName !== '' && courseName !== null) {
                     this.currentCourseObject.courseName = courseName;
                  } else if (courseName === null) {
                     return;
@@ -60,7 +59,9 @@ export default ({
                  }
             }
             
-            image = prompt('Add image url. Make sure it is valid!');
+            let image = prompt('Add image url. Make sure it is valid!');
+            // image.src = url;
+            // image.id = "tableImage";
             this.currentCourseObject.image = image;
 
             // Going around the objects reference type
@@ -69,7 +70,66 @@ export default ({
                 object[key] = this.currentCourseObject[key];
             }
 
+            // console.log(object.image);
+
             this.courseInformationArray.push(object);
+
+        },
+
+        update(event) {
+
+            let isNumber = new RegExp('^[0-9]+$');
+            let courseName = '';
+            let description = '';
+            let lessons = '';
+            let indexOfObject = 0;
+            let id = '';
+
+            while (!isNumber.test(id)) {
+                id = prompt('Enter the ID of the course you want to update');
+                 if (id === null) {
+                    return;
+                 }
+            }
+
+                console.log(id)
+
+                if (this.courseInformationArray.length > 1) {
+                for (let i = 0; i < this.courseInformationArray.length; i++) {
+                    if (id === this.courseInformationArray[i].id) {
+                        indexOfObject = i;
+                    }
+                }
+              }
+            
+
+            console.log(this.courseInformationArray[indexOfObject], indexOfObject)
+
+            while (!courseName) {
+                 courseName = prompt('Add Course Name (required field)?');
+                if (courseName !== '' && courseName !== null) {
+                    this.courseInformationArray[indexOfObject].courseName = courseName;
+                 } else if (courseName === null) {
+                    return;
+                 }
+            }
+
+                description = prompt('Description?');
+                this.courseInformationArray[indexOfObject].description = description;
+
+            while (!lessons || !isNumber.test(lessons)) {
+                 lessons = prompt('Add Lessons(count) (required field and must be a number)?');
+                 if (lessons !== '' && lessons !== null && isNumber.test(lessons)) {
+                    this.courseInformationArray[indexOfObject].lessons = lessons;
+                 } else if (lessons === null) {
+                    return;
+                 }
+            }
+            
+            let image = prompt('Add image url. Make sure it is valid!');
+         
+            this.courseInformationArray[indexOfObject].image = image;
+
 
         }
 
@@ -95,7 +155,7 @@ export default ({
         <main>
 
             <button v-on:click="add">ADD</button>
-            <button v-on:click="greet">UPDATE</button>
+            <button v-on:click="update">UPDATE</button>
             <button v-on:click="greet">DELETE</button>
 
 
@@ -115,7 +175,7 @@ export default ({
                     <td class="tdBorder">{{object.id}}</td>
                     <td class="tdBorder">{{object.courseName}}</td>
                     <td class="tdBorder">{{object.description}}</td>
-                    <td class="tdBorder">ff</td>
+                    <td class="tdBorder">{{object.lessons}}</td>
                     <td class="tdBorder">
                         <select name="" id="">
                         <option value="">Active</option>
@@ -123,7 +183,8 @@ export default ({
                         </select>
                     </td>
                     <td class="tdBorder">ffaaa</td>
-                    <td><img src="{{object.image}}" alt=""></td>
+                    <td><img src={{object.image}} alt=""></td>
+                    <!-- <td><img src="https://avatars.mds.yandex.net/i?id=84dbd50839c3d640ebfc0de20994c30d-4473719-images-taas-consumers&n=27&h=480&w=480" alt=""></td> -->
                 </tr>
                 
             </table>
@@ -168,6 +229,10 @@ export default ({
         border-right: solid black 2px;
     }
 
+    td {
+        text-align: center;
+    }
+
     table {
         width: 100%;
     }
@@ -191,6 +256,10 @@ export default ({
         border: solid black 2px;
         border-radius: 5px;
         margin-right: 20px;
+    }
+
+    #tableImage {
+        /* width: 40%; */
     }
 
 footer {
