97015fbf228cfbb192197961aaa63ccc1f2b5d71
Erol-web <81830899+Erol-web@users.noreply.github.com>
2022-06-24 15:16:18 +0300

    add

--- The-Learning-App/src/views/Learning.vue
+++ The-Learning-App/src/views/Learning.vue
@@ -22,10 +22,10 @@
 <script>
 
 export default ({
-    data () {
+    data() {
         return {
             courseInformationArray: [],
-            currentCourseObject: {id: 0, courseName: '', description: '', lessons: 0, image: undefined }
+            currentCourseObject: { id: 0, courseName: '', description: '', lessons: 0, image: undefined }
         }
     },
 
@@ -39,26 +39,26 @@ export default ({
             // let image = document.createElement('img');
 
             while (!courseName) {
-                 courseName = prompt('Add Course Name (required field)?');
+                courseName = prompt('Add Course Name (required field)?');
                 if (courseName !== '' && courseName !== null) {
                     this.currentCourseObject.courseName = courseName;
-                 } else if (courseName === null) {
+                } else if (courseName === null) {
                     return;
-                 }
+                }
             }
 
-                description = prompt('Description?');
-                this.currentCourseObject.description = description;
+            description = prompt('Description?');
+            this.currentCourseObject.description = description;
 
             while (!lessons || !isNumber.test(lessons)) {
-                 lessons = prompt('Add Lessons(count) (required field and must be a number)?');
-                 if (lessons !== '' && lessons !== null && isNumber.test(lessons)) {
+                lessons = prompt('Add Lessons(count) (required field and must be a number)?');
+                if (lessons !== '' && lessons !== null && isNumber.test(lessons)) {
                     this.currentCourseObject.lessons = lessons;
-                 } else if (lessons === null) {
+                } else if (lessons === null) {
                     return;
-                 }
+                }
             }
-            
+
             let image = prompt('Add image url. Make sure it is valid!');
             // image.src = url;
             // image.id = "tableImage";
@@ -84,111 +84,111 @@ export default ({
             let lessons = '';
             let indexOfObject = 0;
             let id = '';
+            let updatedObject = {};
 
             while (!isNumber.test(id)) {
                 id = prompt('Enter the ID of the course you want to update');
-                 if (id === null) {
+                if (id === null) {
                     return;
-                 }
+                }
             }
 
-                console.log(id)
+            while (!courseName) {
+                courseName = prompt('Add Course Name (required field)?');
+                if (courseName !== '' && courseName !== null) {
+                    updatedObject.courseName = courseName;
+                } else if (courseName === null) {
+                    return;
+                }
+            }
 
-                if (this.courseInformationArray.length > 1) {
+            description = prompt('Description?');
+            updatedObject.description = description;
+
+            while (!lessons || !isNumber.test(lessons)) {
+                lessons = prompt('Add Lessons(count) (required field and must be a number)?');
+                if (lessons !== '' && lessons !== null && isNumber.test(lessons)) {
+                    updatedObject.lessons = lessons;
+                } else if (lessons === null) {
+                    return;
+                }
+            }
+
+            let image = prompt('Add image url. Make sure it is valid!');
+
+            updatedObject.image = image;
+
+            if (this.courseInformationArray.length > 1) {
                 for (let i = 0; i < this.courseInformationArray.length; i++) {
-                    if (id === this.courseInformationArray[i].id) {
+                    if (Number(id) === this.courseInformationArray[i].id) {
                         indexOfObject = i;
                     }
                 }
-              }
-            
-
-            console.log(this.courseInformationArray[indexOfObject], indexOfObject)
-
-            while (!courseName) {
-                 courseName = prompt('Add Course Name (required field)?');
-                if (courseName !== '' && courseName !== null) {
-                    this.courseInformationArray[indexOfObject].courseName = courseName;
-                 } else if (courseName === null) {
-                    return;
-                 }
             }
 
-                description = prompt('Description?');
-                this.courseInformationArray[indexOfObject].description = description;
+            updatedObject.id = indexOfObject + 1;
 
-            while (!lessons || !isNumber.test(lessons)) {
-                 lessons = prompt('Add Lessons(count) (required field and must be a number)?');
-                 if (lessons !== '' && lessons !== null && isNumber.test(lessons)) {
-                    this.courseInformationArray[indexOfObject].lessons = lessons;
-                 } else if (lessons === null) {
-                    return;
-                 }
-            }
-            
-            let image = prompt('Add image url. Make sure it is valid!');
-         
-            this.courseInformationArray[indexOfObject].image = image;
-
+            this.courseInformationArray.splice(indexOfObject, 1, updatedObject);
 
         }
 
-       
+
 
 
     }
-    
+
 })
 
 </script>
 
  <template>
-    
-     <body>
-        
+
+    <body>
+
         <header>
             <h2>Welcome</h2>
             <img src="../assets/User-Icon-PNG.png" alt="">
-            
+
         </header>
 
         <main>
 
-            <button v-on:click="add">ADD</button>
-            <button v-on:click="update">UPDATE</button>
-            <button v-on:click="greet">DELETE</button>
-
+            <div id="buttonsDiv">
+                <button v-on:click="add">ADD</button>
+                <button v-on:click="update">UPDATE</button>
+                <button v-on:click="greet">DELETE</button>
+            </div>
 
             <table>
-                
+
                 <tr>
-                   <th class="thBorder">ID</th>
-                   <th class="thBorder">Course Name</th>
-                   <th class="thBorder">Description</th>
-                   <th class="thBorder">Lessons(count)</th>
-                   <th class="thBorder">State</th>
-                   <th class="thBorder">Date added</th>
-                   <th id="thImage">Image</th>
+                    <th class="thBorder">ID</th>
+                    <th class="thBorder">Course Name</th>
+                    <th class="thBorder">Description</th>
+                    <th class="thBorder">Lessons(count)</th>
+                    <th class="thBorder">State</th>
+                    <th class="thBorder">Date added</th>
+                    <th id="thImage">Image</th>
                 </tr>
 
                 <tr v-for="object in courseInformationArray">
-                    <td class="tdBorder">{{object.id}}</td>
-                    <td class="tdBorder">{{object.courseName}}</td>
-                    <td class="tdBorder">{{object.description}}</td>
-                    <td class="tdBorder">{{object.lessons}}</td>
+                    <td class="tdBorder">{{ object.id }}</td>
+                    <td class="tdBorder">{{ object.courseName }}</td>
+                    <td class="tdBorder">{{ object.description }}</td>
+                    <td class="tdBorder">{{ object.lessons }}</td>
                     <td class="tdBorder">
                         <select name="" id="">
-                        <option value="">Active</option>
-                        <option value="">Archive</option>
+                            <option value="">Active</option>
+                            <option value="">Archive</option>
                         </select>
                     </td>
                     <td class="tdBorder">ffaaa</td>
                     <td><img src={{object.image}} alt=""></td>
                     <!-- <td><img src="https://avatars.mds.yandex.net/i?id=84dbd50839c3d640ebfc0de20994c30d-4473719-images-taas-consumers&n=27&h=480&w=480" alt=""></td> -->
                 </tr>
-                
+
             </table>
-               
+
         </main>
 
         <footer>
@@ -203,84 +203,115 @@ export default ({
 <style scoped>
 @import '../assets/base.css';
 
-    body {
-        display: grid;
-        grid-template-areas: 
-            "header"
-            "main"
-            "footer";
-            margin: 0;
-    }
+body {
+    display: grid;
+    grid-template-areas:
+        "header"
+        "main"
+        "footer";
+    min-height: 100vh;
+}
 
-    table, tr {
-        border: solid black 2px;
-    }
+#buttonsDiv {
+    display: flex;
+    justify-content: space-evenly;
+    margin-bottom: 20px;
+}
 
-    .thBorder {
-        border-right: solid black 2px;
-        border-bottom: solid black 2px;
-    }
+button {
+    width: 100px;
+    padding-top: 5px;
+    padding-bottom: 5px;
+    font-size: 1rem;
+    font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+    font-weight: 600;
+    background-color: peachpuff;
+}
 
-    #thImage {
-        border-bottom: solid black 2px;
-    }
+button:hover {
+    background-color: darkturquoise;
+    cursor:crosshair;
+}
 
-    .tdBorder {
-        border-right: solid black 2px;
-    }
+table {
+    background-color: aquamarine;
+    font-size: 0.9rem;
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+}
 
-    td {
-        text-align: center;
-    }
+table,
+tr {
+    border: solid black 2px;
+}
 
-    table {
-        width: 100%;
-    }
+.thBorder {
+    border-right: solid black 2px;
+    border-bottom: solid black 2px;
+}
 
-    header {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        background-color: cadetblue;
-        height: 70px;
-        width: 100%;
-    }
+#thImage {
+    border-bottom: solid black 2px;
+}
 
-    header h2 {
-        margin-left: 20px;
-    }
+.tdBorder {
+    border-right: solid black 2px;
+}
 
-    img {
-        width: 3.5%;
-        height: 70%;
-        border: solid black 2px;
-        border-radius: 5px;
-        margin-right: 20px;
-    }
+td {
+    text-align: center;
+}
 
-    #tableImage {
-        /* width: 40%; */
-    }
+table {
+    width: 100%;
+}
+
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: cadetblue;
+    height: 70px;
+    width: 100%;
+}
+
+header h2 {
+    margin-left: 20px;
+}
+
+img {
+    width: 3.5%;
+    height: 70%;
+    border: solid black 2px;
+    border-radius: 5px;
+    margin-right: 20px;
+}
+
+#tableImage {
+    /* width: 40%; */
+}
 
 footer {
-  background-color: cadetblue;
-  grid-area: footer;
-  display: flex;
-  justify-content: space-evenly;
-  align-items: center;
+    background-color: cadetblue;
+    grid-area: footer;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    height: 10vh;
+    position: absolute;
+    bottom: 0;
+    width: 100%;
 }
 
 .links {
-  text-decoration: none;
-  color: black;
-  font-size: 1.3rem;
-  font-weight: bold;
+    text-decoration: none;
+    color: black;
+    font-size: 1.3rem;
+    font-weight: bold;
 }
 
 .links:hover {
- background-color: yellow;
- border-radius: 8px;
- 
-}
+    background-color: yellow;
+    border-radius: 8px;
 
+}
 </style>
